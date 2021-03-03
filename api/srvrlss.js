fce7d4163d85776f6d7d795a7f154456eab272d5
bluffitee <69552200+bluffitee@users.noreply.github.com>
2021-03-03 03:22:59 -0600

    Update srvrlss.js

--- api/srvrlss.js
+++ api/srvrlss.js
@@ -1,9 +1,8 @@
 module.exports = (req, res) => {
-  const date = new Date().toString();
-  fetch("https://programming-quotes-api.herokuapp.com/quotes")
-  .then(res => res.json())
-  .then(data => {
-    res.status(200).send(data);
-  })
-  .catch(err => console.log(err));
+  let data;
+  
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => data = json)
+  res.status(200).send(data);
 };
